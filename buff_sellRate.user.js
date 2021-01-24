fa7e87e59af76a0625b4221889905f8ba17a78ce
韩松 <Pronax.han@gmail.com>
2021-01-25 00:05:30 +0800

    添加主页显示求购比例

--- buff_sellRate.user.js
+++ buff_sellRate.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         网易BUFF价格比例(找挂刀)插件
 // @namespace    http://pronax.wtf/
-// @version      2021-1-22 23:22:11
-// @description  try to take over the world! --Written by Pronax
+// @version      2021-1-24 22:38:11
+// @description  你的挂刀得力助手，有问题反馈可以直接发邮件给我（funkyturkey@yeah.net）在greasy上发issue也可以，不过可能响应比较慢
 // @copyright    2020, Pronax
 // @author       Pronax
 // @license      MIT
@@ -18,6 +18,10 @@
 (function () {
     'use strict';
 
+
+
+    // 	-----自定义参数--------start---------------------
+
     const min_range = 0.63;                     // 比例取值最小范围，小于等于这个值的比例会直接渲染成最小值渐变色
     const needSort = 1;                         // 是否自动按比例排序      需要：1    不需要：0
     const sortType = 1;                         // 排序规则    从低到高（升序）：1    从高到低（降序）：0
@@ -26,19 +30,28 @@
     var market_color_high = "80,39,255";        // 最大值渐变色，比例越接近最大值（默认是1）会越趋近这个颜色，格式：['r','g','b'] 或者 "r,g,b"
     var market_color_low = "255,30,30";         // 最小值渐变色，比例越接近最小值（默认是0.63）会越趋近这个颜色，格式：['r','g','b'] 或者 "r,g,b"
 
+    // 	-----自定义参数--------end-----------------------
+
+
+
     // 处理成数组
     if (!Array.isArray(market_color_high)) market_color_high = market_color_high.split(",");
     if (!Array.isArray(market_color_low)) market_color_low = market_color_low.split(",");
 
-    // 饰品页面的渐变色，用法同上，因为是深色的背景所以效果不是很好，改成了固定色阶显示，代码没删，喜欢渐变的可以把色阶删掉，把渐变的注释打开就能用了
-    //     var goods_color_high = "160,255,197";
-    //     var goods_color_low = "255,45,73";
-    // 处理成数组
-    //     if(!Array.isArray(goods_color_high))goods_color_high = goods_color_high.split(",");
-    //     if(!Array.isArray(goods_color_low))goods_color_low = goods_color_low.split(",");
-
     const alertColor = Array(0, 255, 72);
     const backgroundColor = Array(69, 83, 108);
+    const steanOrderScaleTemp = "<span class=\"f_12px f_Bold l_Right\" style=\"margin-top: inherit;opacity: 0.8;\"></span>";
+
+    function getUrlParam(name, url) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var result
+        if (url) {
+            result = url.substr(34).match(reg);  //匹配目标参数
+        } else {
+            result = window.location.search.substr(1).match(reg);  //匹配目标参数
+        }
+        if (result != null) return unescape(result[2]); return null; //返回参数值
+    }
 
     function sortGoods(isAsc) {
         $("#j_list_card>ul>li").sort(function (a, b) {
@@ -54,8 +67,16 @@
     }
 
     // 保留2位小数
-    function roundToTwo(num, status) { // status不为false时四舍五入
-        return status ? Math.round((num * 100) + 0.5) / 100 : Math.round((num * 100)) / 100;
+    function roundToTwo(num, upOrDown) { // upOrDown为true时四舍五入
+        return upOrDown ? Math.round((num * 100) + 0.5) / 100 : Math.round((num * 100)) / 100;
+    }
+
+    function getWithoutFeePrice(originPrice, upOrDown) {
+        return roundToTwo(originPrice / 1.15, upOrDown);
+    }
+
+    function getScale(originPrice, withFeePrice, upOrDown) {
+        return roundToTwo(originPrice / (withFeePrice / 1.15), upOrDown);
     }
 
     function shade(obj, prop, color1, color2, mills) {
@@ -87,13 +108,42 @@
         return max >= min ? f * (max - min) + min : (1 - f) * (min - max) + max;
     }
 
-    function getItemId() {
+    function paintingGradient(scale, target, position, targetTemplate) {
+        var template;
+        if (targetTemplate) {
+            template = targetTemplate;
+        } else {
+            template = '<strong class="f_Strong price_scale" style="float: right;"></strong>';
+        }
+        let red = gradient(market_color_high[0], market_color_low[0], scale);
+        let green = gradient(market_color_high[1], market_color_low[1], scale);
+        let blue = gradient(market_color_high[2], market_color_low[2], scale);
+        switch (position) {
+            case 1:
+                $(target).before($(template).css("color", "rgb(" + red + "," + green + "," + blue + ")").text(scale));
+                break;
+            case 2:
+                $(target).prepend($(template).css("color", "rgb(" + red + "," + green + "," + blue + ")").text(scale));
+                break;
+            case 3:
+                $(target).append($(template).css("color", "rgb(" + red + "," + green + "," + blue + ")").text(scale));
+                break;
+            case 4:
+                $(target).after($(template).css("color", "rgb(" + red + "," + green + "," + blue + ")").text(scale));
+                break;
+            default:
+                $(target).append($(template).css("color", "rgb(" + red + "," + green + "," + blue + ")").text(scale));
+        }
+    }
+
+    function getItemId(steamLink, url) {
         return new Promise(function (resolve, reject) {
-            let buff_item_id = getUrlParam("goods_id");
+            let buff_item_id = getUrlParam("goods_id", url);
             let steam_item_id = GM_getValue(buff_item_id);
             if ((!steam_item_id) || steam_item_id.length > 20 || steam_item_id == buff_item_id) {
+                console.log("itemID未知");
                 GM_xmlhttpRequest({
-                    url: document.getElementsByClassName("detail-summ")[0].lastElementChild.href,
+                    url: steamLink,
                     method: "get",
                     onload: function (res) {
                         if (res.status == 200) {
@@ -120,13 +170,32 @@
         });
     }
 
-    function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-        if (r != null) return unescape(r[2]); return null; //返回参数值
+    function getSteamOrderList(steamLink, url) {
+        return new Promise(function (resolve, reject) {
+            getItemId(steamLink, url).then(function onFulfilled(itemId) {
+                GM_xmlhttpRequest({
+                    url: window.location.protocol + "//steamcommunity.com/market/itemordershistogram?country=CN&language=schinese&currency=23&item_nameid=" + itemId + "&two_factor=0",
+                    method: "get",
+                    onload: function (res) {
+                        if (res.status == 200) {
+                            resolve(JSON.parse(res.response));
+                        } else {
+                            console.log("访问steamorder列表出错：", res);
+                        }
+                    },
+                    onerror: function (err) {
+                        console.log("访问steamorder列表出错：", err);
+                        reject(err);
+                    }
+                });
+            }).catch(function onRejected(err) {
+                console.log('获取itemID错误：', err);
+                reject(err);
+            });
+        });
     }
 
-    // 商品
+    // 商品详情
     window.buff_csgo_goods_scale_plugin_load = function (steam_price) {
         // 检测商品是否加载完成
         if ($("#market-selling-list").length == 0) {
@@ -139,28 +208,12 @@
         if (!steam_price) {
             steam_price = parseFloat($(price_list[isLogined ? 1 : 0]).text().split("(")[0].slice(1));
         }
-        let basic_price = roundToTwo(steam_price / 1.15, true);
+        let basic_price = getWithoutFeePrice(steam_price, true);
         for (let i = isLogined ? 2 : 1; i < price_list.length; i++) {
             let seller_price = price_list[i].innerText.slice(1);
             let scale = roundToTwo(seller_price / basic_price);
             if (i == (isLogined ? 2 : 1)) {
                 $(".f_Strong .hide-usd")[0].innerText = basic_price;
-
-                // 渐变色部分 --------------------------------------------------------------------------------------- start
-
-                //    let red = gradient(goods_color_high[0],goods_color_low[0],scale);
-                //    let green = gradient(goods_color_high[1],goods_color_low[1],scale);
-                //    let blue = gradient(goods_color_high[2],goods_color_low[2],scale);
-                //    if($(".good_scale").length==0){
-                //        $(price_list[isLogined?1:0]).append($("<big class='good_scale' style='color: rgb("+red+" "+green+" "+blue+");margin-left: 6px'>"+scale+"</big>"));
-                //    }else{
-                //        $(".good_scale").text(scale).css("color","rgb("+red+" "+green+" "+blue+")");
-                //    }
-
-                // 渐变色部分 --------------------------------------------------------------------------------------- end
-
-                // 色阶部分 --------------------------------------------------------------------------------------- start
-
                 let color;
                 switch (true) {
                     case scale > 0.9: color = "#a0ffc5"; break;
@@ -171,53 +224,20 @@
                 }
                 if ($(".good_scale").length == 0) {
                     $(price_list[isLogined ? 1 : 0]).append($("<big class='good_scale' style='color: " + color + ";margin-left: 6px'>" + scale + "</big>"));
-                    getItemId().then(function onFulfilled(itemId) {
-                        GM_xmlhttpRequest({
-                            url: window.location.protocol + "//steamcommunity.com/market/itemordershistogram?country=CN&language=schinese&currency=23&item_nameid=" + itemId + "&two_factor=0",
-                            method: "get",
-                            onload: function (res) {
-                                if (res.status == 200) {
-                                    let js = JSON.parse(res.response);
-                                    $(".detail-cont").append("<div id='steam_order'>" + js.buy_order_summary + "</div>");
-                                    window.market_listing_price_with_fee = js.buy_order_graph[0][0];
-                                    $(".market_commodity_orders_header_promote:last").after("<small class='market_listing_price_with_fee'>" + roundToTwo(seller_price / (window.market_listing_price_with_fee / 1.15), true) + "</small>");
-                                } else {
-                                    console.log("访问steamorder列表出错：", res);
-                                }
-                            },
-                            onerror: function (err) {
-                                console.log("访问steamorder列表出错：", err);
-                            }
-                        });
-                    }).catch(function onRejected(error) {
-                        console.log('获取itemID错误：', error);
+                    let steamLink = document.getElementsByClassName("detail-summ")[0].lastElementChild.href;
+                    getSteamOrderList(steamLink).then(function onFulfilled(json) {
+                        $(".detail-cont").append("<div id='steam_order'>" + json.buy_order_summary + "</div>");
+                        window.market_listing_price_with_fee = json.highest_buy_order / 100;
+                        $(".market_commodity_orders_header_promote:last").after("<small class='market_listing_price_with_fee'>" + getScale(seller_price, window.market_listing_price_with_fee, true) + "</small>");
                     });
                 } else {
                     $(".good_scale").text(scale).css("color", color);
-                    console.log("变了");
-                    $(".market_listing_price_with_fee").text(roundToTwo(seller_price / (window.market_listing_price_with_fee / 1.15), true));
+                    $(".market_listing_price_with_fee").text(getScale(seller_price, window.market_listing_price_with_fee, true));
                 }
-
-                // 色阶部分 --------------------------------------------------------------------------------------- end
             }
             $(price_list[i].parentNode).next().append($("<b>" + scale + "</b>"));
         }
         $("#market-selling-list").addClass("calculated");
-
-        // 测试用 发布时删除 start
-        /*         for(let j=1;j>0.6;j-=0.03){
-            let color;
-            switch(true){
-                case j>0.9: color = "#a0ffc5";break;
-                case j>0.8: color = "#b8ff8a";break;
-                case j>=0.73: color = "#fff054";break;
-                case j>=0.67: color = "#ff7e15";break;
-                default : color = "#ff0049";break;
-            }
-            $(".detail-header").append($("<big class='good_scale' style='color: "+color+";margin-left: 6px;font-weight:bold'>"+roundToTwo(j)+"</big>"));
-        } */
-        // 测试用 发布时删除 end
-
     }
 
     // 列表
@@ -235,18 +255,21 @@
             let target = $(goods[i]).find("p>strong.f_Strong")[0];
             let buff_price = target.innerText.slice(2);
             let url = $(goods[i]).children("a")[0].href;
+            //	buff的ajax
             $.ajax({
                 url: url,
                 method: "get",
                 success: function (data) {
                     status--;
                     let steam_price = $(data).find(".detail-summ .f_Strong>span.custom-currency")[0].getAttribute('data-price');
-                    let scale = roundToTwo(buff_price / (steam_price / 1.15), true);
-                    let red = gradient(market_color_high[0], market_color_low[0], scale);
-                    let green = gradient(market_color_high[1], market_color_low[1], scale);
-                    let blue = gradient(market_color_high[2], market_color_low[2], scale);
-                    $(target).append($('<strong class="f_Strong price_scale" style="color: rgb(' + red + ' ' + green + ' ' + blue + ');float: right;">' + scale + '</strong>'));
+                    let scale = getScale(buff_price, steam_price, true);
                     $(goods[i]).attr("data-sort", scale);
+                    paintingGradient(scale, target, 3);
+                    let steam_link = $(data).find(".detail-summ>a")[0].href;
+                    getSteamOrderList(steam_link, url).then(function onFulfilled(json) {
+                        let steamOrderScale = getScale(buff_price, (json.highest_buy_order / 100), true);
+                        paintingGradient(steamOrderScale, target, 4, steanOrderScaleTemp);
+                    });
                     if (needSort) {
                         sortGoods(sortType);
                     }
@@ -263,10 +286,10 @@
         $("#j_list_card").addClass("calculated");
     }
 
-    if (location.pathname === "/market/goods") {
-        GM_addStyle(".market_commodity_orders_header_promote {color: whitesmoke;}#steam_order{margin-top:5px}.market_listing_price_with_fee{color: #9c6c0d;font-size: 12px;margin-left: 6px;}");
-        buff_csgo_goods_scale_plugin_load();
 
+    if (location.pathname === "/market/goods") {
+        buff_csgo_goods_scale_plugin_load();
+        GM_addStyle(".market_commodity_orders_header_promote {color: whitesmoke;}#steam_order{margin-top:5px}.market_listing_price_with_fee{color: #9c6c0d;font-size: 12px;margin-left: 6px;}");
         setTimeout(function () {
             $(document).ajaxSuccess(function (event, status, header, result) {
                 if (header.url.slice(0, 28) === "/api/market/goods/sell_order") {
