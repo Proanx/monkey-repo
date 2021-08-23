9631751cc15c27daea296ff25363725786a4a75d
韩松 <Pronax.han@gmail.com>
2021-08-23 22:56:54 +0800

    现在真的可以自动登录了

--- javAutoLogin.user.js
+++ javAutoLogin.user.js
@@ -1,19 +1,54 @@
 // ==UserScript==
 // @name         JAV自动登录
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @exclude      https://www.javlibrary.com/cn/login.php
 // @match        https://www.javlibrary.com/cn/*
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    var isLogin = $(".menutext").children().length==4;
-    if(isLogin){}else{
-        window.open("/cn/login.php", "_blank", "width=600,height=400");
+    switch (location.pathname) {
+        case "/cn/logout.php":
+        case "/cn/login.php":
+            return;
+        case "/cn/myaccount.php":
+            GM_setValue("javCookie", document.cookie);
+            break;
+        default:
+            if (!document.cookie.match(/userid/)) {
+                let savedCookie = GM_getValue("javCookie");
+                if (savedCookie && savedCookie.match(/userid/)) {
+                    let ca = savedCookie.split(';');
+                    for (let i = 0; i < ca.length; i++) {
+                        setCookie(ca[i]);
+                    }
+                } else {
+                    window.open("/cn/login.php", "_blank", "width=600,height=400");
+                }
+            }
+            break;
     }
+
+    $(".menutext>a[href='logout.php']").click(() => {
+        GM_setValue("javCookie", 0);
+        let timeStr = new Date(0).toGMTString();
+        var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+        if (keys) {
+            for (let key of keys) {
+                document.cookie = key + '=0;expires=' + timeStr;
+            }
+        }
+    });
+
+    function setCookie(cEntry) {
+        let d = new Date();
+        if (cEntry.match(/timezone/)) { return; }
+        document.cookie = cEntry + "; path=/";
+    }
+
 })();
