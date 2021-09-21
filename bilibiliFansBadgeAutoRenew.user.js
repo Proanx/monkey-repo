a63a7bfe5a644da491c1e22c8d93e8ae723dd356
韩松 <Pronax.han@gmail.com>
2021-09-21 16:39:32 +0800

    修复一些bug

--- bilibiliFansBadgeAutoRenew.user.js
+++ bilibiliFansBadgeAutoRenew.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站自动续牌
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://t.bilibili.com/
@@ -17,13 +17,16 @@
 
     var totalCount;
     var doneCount;
-    var failCount = 0;
+    var failedList = new Map();
+
+    var emojiList = ["打卡(˘･_･˘)", "打卡(。’▽’。)♡", "打卡⊙ω⊙", "打卡ヾ(✿ﾟ▽ﾟ)ノ", "打卡_(:3」∠)_"];
 
     var curPage;
     var totalPages;
 
     setTimeout(() => {
         if (GM_getValue("timestamp") != new Date().toLocaleDateString()) {
+            console.log("今天日子不对啊");
             init();
         }
     }, 1000);
@@ -43,14 +46,18 @@
                 doneCount = 0;
                 totalCount = result.data.count;
                 curPage = result.data.pageinfo.curPage;
-                totalPages = result.data.pageinfo.totalPages;
-                let count = 1;
+                totalPages = result.data.pageinfo.totalpages;
+                let count = 0;
                 for (let i of result.data.fansMedalList) {
                     if (i.today_feed < 100) {
+                        console.log(`预计 ${count * 1500} 毫秒后给 ${i.target_name} 发送弹幕`);
                         setTimeout(() => {
                             sendMsg(i.roomid);
-                        }, count++ * 1500);
+                        }, count++ * 3000);
+                    } else {
+                        doneCount++;
                     }
+                    afterDone();
                 }
             },
             error: function (e) {
@@ -60,17 +67,19 @@
         });
     }
 
-    function sendMsg(roomId, msg = "(´･_･`)") {
+    function sendMsg(roomId) {
+        let times = failedList.get(roomId) || 0;
+        failedList.delete(roomId);
         $.ajax({
             url: "https://api.live.bilibili.com/msg/send",
             type: "POST",
             data: {
                 "bubble": 0,
-                "msg": msg,
+                "msg": roomId == 21470918 ? "王哥我爱你王哥" : emojiList[(Math.random() * 200 >> 1) % emojiList.length],
                 "color": 16777215,
                 "mode": 1,
                 "fontsize": 25,
-                "rnd": Math.round(new Date() / 1000),
+                "rnd": Math.round(new Date() / 1000) - 3,
                 "roomid": roomId,
                 "csrf": jct,
                 "csrf_token": jct
@@ -79,31 +88,44 @@
                 withCredentials: true //允许跨域带Cookie
             },
             success: function (result) {
-                switch (result.code) {
-                    case 0:
-                        if (++doneCount == totalCount) {
-                            if (curPage >= totalPages) {
-                                GM_setValue("timestamp", new Date().toLocaleDateString());
-                            } else {
-                                init(++curPage);
-                            }
-                        }
-                        break;
-                    case 10030:
-                        setTimeout(() => {
-                            failCount && failCount--;
-                            sendMsg(roomId);
-                        }, ++failCount * 3000);
-                        break;
-                    default:
-                        alert("发送弹幕失败");
-                        console.log("发送弹幕失败：", e);
+                console.log(result);
+                if (result.code || result.msg != "") {
+                    failedList.set(roomId, times);
+                } else {
+                    doneCount++;
                 }
+                afterDone();
             },
             error: function (e) {
                 console.log("发送弹幕失败：", e);
+                alert("发送弹幕失败");
             }
         });
+    }
+
+    function afterDone() {
+        if (doneCount + failedList.size == totalCount) {
+            if (doneCount == totalCount) {
+                if (curPage >= totalPages) {
+                    console.log('都搞定了');
+                    GM_setValue("timestamp", new Date().toLocaleDateString());
+                } else {
+                    init(++curPage);
+                }
+                return;
+            }
+            for (let i of failedList) {
+                let count = 1;
+                if (i[1] <= 2) {
+                    failedList.set(i[0], ++i[1]);
+                    setTimeout(() => {
+                        sendMsg(i[0]);
+                    }, count++ * 3000);
+                } else {
+                    console.log("发送失败", i[0]);
+                }
+            }
+        }
     }
 
     function getJct() {
