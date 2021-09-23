cef1bb799adc48c86ca688af8d5f5d6a07bd6901
韩松 <Pronax.han@gmail.com>
2021-09-24 00:24:31 +0800

    可以在代码里修改持久化设置了，优化了礼物显示，修复一些bug

--- bilibiliLiveSimplify.user.js
+++ bilibiliLiveSimplify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站直播聊天室简化
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.2
 // @description  简化聊天室但保留了粉丝牌、房管、老爷等标志。
 // @author       Pronax
 // @include      /https:\/\/live\.bilibili\.com\/(blanc\/)?\d+/
@@ -13,23 +13,39 @@
 (function () {
 	'use strict';
 
-	GM_addStyle(".block-effect-ctnr{min-width: 148px;}.live-room-app .app-content{padding-top:60px !important}");
+	GM_addStyle(".block-effect-ctnr{min-width: 175px;}.live-room-app .app-content{padding-top:60px !important}");
 	// 弹幕
-	GM_addStyle(".chat-history-panel .chat-history-list .chat-item.top3-notice{width:268px;border-radius:3px;margin:0}.fansmedal-popover{margin-left:-124px}.fansmedal-popover::after,.fansmedal-popover::before{left:80%!important}.rank-icon{float:right;margin:0 0 0 5px!important;width:24px!important;height:16px!important}.chat-item.danmaku-item>.user-name{color:#bbb!important;display:flex}.chat-item.danmaku-item>.title-label{display:none}.chat-item.danmaku-item>.danmaku-content{display:block!important;font-size:14px}.chat-item.danmaku-item:not(.superChat-card-detail)>div,.chat-item.danmaku-item:not(.superChat-card-detail)>.vip-icon{float:right;margin:0 0 0 5px!important}.chat-item.gift-item{margin:4px 0;border:1px solid #ffa32829;background-color:#ffc1001a}.chat-item.danmaku-item:not(.superChat-card-detail){margin:4px 0!important;border:1px solid #fff;background-color:#fff!important}.chat-item.gift-item,.chat-item.danmaku-item:not(.superChat-card-detail){width:268px;border-radius:5px}.chat-item.gift-item>.fans-medal-item-ctnr,.chat-item.important-prompt-item>.fans-medal-item-ctnr{float:right;right:-4px}.chat-item.superChat-card-detail .name{color:#444!important;font-size:16px!important}.chat-item.superChat-card-detail .fans-medal-item-ctnr{position:absolute;right:10px;top:13px}.chat-item.superChat-card-detail>.card-item-top-right{color:#fff!important;right:110px!important;border:1px solid;padding-right:6px!important;background-color:#2a60b2}.chat-item.superChat-card-detail .card-item-middle-top{height:32px!important}.chat-history-panel .danmaku-buffer-prompt{bottom:145px;border-radius:20px;width:130px;opacity:.85;margin:0 auto}");
+	GM_addStyle(".chat-history-panel .chat-history-list .chat-item.top3-notice{width:268px;border-radius:3px;margin:0}.fansmedal-popover{margin-left:-124px}.fansmedal-popover::after,.fansmedal-popover::before{left:80%!important}.rank-icon{float:right;margin:0 0 0 5px!important;width:24px!important;height:16px!important}.chat-item.danmaku-item>.user-name{color:#bbb!important;display:flex}.chat-item.danmaku-item>.title-label{display:none}.chat-item.danmaku-item>.danmaku-content{display:block!important;font-size:14px}.chat-item.danmaku-item:not(.superChat-card-detail)>div,.chat-item.danmaku-item:not(.superChat-card-detail)>.vip-icon{float:right;margin:0 0 0 5px!important}.chat-item.gift-item{margin:4px 0;border:1px solid #ffa32829;background-color:#ffc1001a}.chat-item.danmaku-item:not(.superChat-card-detail){margin:4px 0!important;border:1px solid #fff;background-color:#fff!important}.chat-item.gift-item,.chat-item.danmaku-item:not(.superChat-card-detail){width:268px;border-radius:5px}.chat-item.gift-item>.fans-medal-item-ctnr,.chat-item.important-prompt-item>.fans-medal-item-ctnr{float:right;right:-4px}.chat-item.superChat-card-detail .name{color:#444!important;font-size:16px!important}.chat-item.superChat-card-detail .fans-medal-item-ctnr{position:absolute;right:10px;top:13px}.chat-item.superChat-card-detail>.card-item-top-right{color:#fff!important;right:110px!important;border:1px solid;padding-right:6px!important;background-color:#2a60b2}.chat-item.superChat-card-detail .card-item-middle-top{height:32px!important}.chat-history-panel .danmaku-buffer-prompt{bottom:60px;position: relative;border-radius:20px;width:130px;opacity:.85;margin:0 auto}");
 	GM_addStyle(".send-ts{;background-color: #fff;opacity:0}.send-ts:hover{;background-color: #fff;opacity:1}");
 	// 标题
 	GM_addStyle(".head-info-section{height:78px!important}.head-info-section>.header-info-ctnr{padding:6px}.head-info-section .right-ctnr{right:5px}");
-	// 入场
-	GM_addStyle(".brush-prompt,.welcome-section-bottom{display:none}");
-	// 礼物
-	GM_addStyle("#gift-control-vm{height: 120px;}.gift-control-panel {height: 110px!important;}");
-	GM_addStyle(".important-prompt-item>.username{margin-right: 4px;color: #999;overflow: hidden;text-overflow: ellipsis;}.important-prompt-item>.count{vertical-align: middle;}");
-	GM_addStyle(".penury-gift-msg{display:none}");
-	// 消除所有提示
-	GM_addStyle(".chat-history-panel .chat-history-list{height: 100% !important}");
 
 	var count = 100;
 	var userId = getUserID();
+	var config = {
+		enterPersistence: true,		// 改为任意false值就可以取消持久化入场信息
+		giftPersistence: true,		// 同上，用来取消持久化礼物
+	}
+
+	if (config.enterPersistence && config.giftPersistence) {
+		GM_addStyle(".chat-history-panel .chat-history-list{height: 100% !important}");	// 消除所有滚动提示
+
+		GM_addStyle(".brush-prompt,.welcome-section-bottom{display:none}");
+
+		GM_addStyle("#gift-control-vm{height: 120px;}.gift-control-panel {height: 110px!important;}");
+		GM_addStyle(".important-prompt-item>.username{margin-right: 4px;color: #999;overflow: hidden;text-overflow: ellipsis;}.important-prompt-item>.count{vertical-align: middle;}");
+		GM_addStyle(".penury-gift-msg{display:none}");
+	} else if (config.enterPersistence) {
+		// 入场		一并移除了入场特效
+		GM_addStyle(".brush-prompt,.welcome-section-bottom{display:none}");
+		GM_addStyle(".chat-history-panel .chat-history-list.with-brush-prompt{height:100%}.chat-history-panel .chat-history-list.with-penury-gift.with-brush-prompt{height:calc(100% - 32px)}");
+	} else if (config.giftPersistence) {
+		// 礼物
+		GM_addStyle(".penury-gift-msg{display:none}");
+		GM_addStyle("#gift-control-vm{height: 120px;}.gift-control-panel {height: 110px!important;}");
+		GM_addStyle(".important-prompt-item>.username{margin-right: 4px;color: #999;overflow: hidden;text-overflow: ellipsis;}.important-prompt-item>.count{vertical-align: middle;}");
+		GM_addStyle(".chat-history-panel .chat-history-list.with-penury-gift{height:100%}.chat-history-panel .chat-history-list.with-penury-gift.with-brush-prompt{height:calc(100% - 30px)}");
+	}
 
 	if (userId) {
 		GM_addStyle(`.chat-item.danmaku-item[data-uid="${userId}"]{border-color: #e4e4e4;margin: 3px 0 !important;`);
@@ -60,28 +76,58 @@
 				}
 			}, true);
 			//  入场信息持久化
-			document.querySelector("#brush-prompt").addEventListener('DOMNodeInserted', function (e) {
-				let ele = document.createElement("div");
-				ele.className = "chat-item important-prompt-item";
-				ele.innerHTML = e.target.innerHTML;
-				document.querySelector(".chat-items").append(ele);
-			}, true);
+			if (config.enterPersistence) {
+				document.querySelector("#brush-prompt").addEventListener('DOMNodeInserted', function (e) {
+					let ele = document.createElement("div");
+					ele.className = "chat-item important-prompt-item";
+					ele.innerHTML = e.target.innerHTML;
+					document.querySelector(".chat-items").append(ele);
+				}, true);
+			}
 			//  礼物信息持久化
-			document.querySelector("#penury-gift-msg").addEventListener('DOMNodeInserted', function (e) {
-				let ele = document.createElement("div");
-				ele.className = "chat-item important-prompt-item";
-				ele.innerHTML = e.target.innerHTML.replace(/class="count">X (\d+)/,"class='count'>×$1");
-				document.querySelector(".chat-items").append(ele);
-			}, true);
+			if (config.giftPersistence) {
+				var giftInfo = new Map();
+				document.querySelector("#penury-gift-msg").addEventListener('DOMNodeInserted', function (e) {
+					let info = e.target.innerText.match(/(.*)X (\d+)/);
+					if (giftInfo.get(info[1]) != undefined) {
+						let latestInfo = giftInfo.get(info[1]);
+						latestInfo.number += +info[2];
+						clearTimeout(latestInfo.timeoutId);
+						latestInfo.timeoutId = setTimeout(() => {
+							let ele = document.createElement("div");
+							ele.className = "chat-item important-prompt-item";
+							ele.innerHTML = e.target.innerHTML.replace(/class="count">X (\d+)/, `class='count'>共${latestInfo.number}个`);
+							document.getElementById(latestInfo.id) && document.getElementById(latestInfo.id).remove();
+							document.querySelector(".chat-items").append(ele);
+							giftInfo.delete(info[1]);
+						}, 2000);
+						giftInfo.set(info[1], latestInfo);
+					} else {
+						let randomId = `gift-msg-${Math.random() * 2000 >> 1}`;
+						giftInfo.set(info[1], {
+							number: +info[2],
+							id: randomId,
+							timeoutId: null
+						});
+						let ele = document.createElement("div");
+						ele.id = randomId;
+						ele.className = "chat-item important-prompt-item";
+						ele.innerHTML = e.target.innerHTML.replace(/class="count">X (\d+)/, "class='count'>×$1");
+						document.querySelector(".chat-items").append(ele);
+					}
+				}, true);
+			}
 			// 	弹幕弹框时间戳
 			let ele = document.createElement("div");
 			ele.setAttribute("id", "send-ts");
 			document.querySelector(".danmaku-menu").append(ele);
 			document.querySelector(".chat-items").addEventListener('click', function (e) {
-				if (e.target.getAttribute("class") && e.target.getAttribute("class").match(/(danmaku\-content|user\-name)/) && document.querySelector(".danmaku-menu").style.display == "none") {
-					let ts = new Date(e.target.parentElement.getAttribute("data-ts") * 1000);
-					document.querySelector("#send-ts").innerText = ts.toLocaleString('chinese', { hour12: false });
-					document.querySelector("#send-ts").innerHTML += "<br/>" + formatDate(Math.floor((new Date() - ts) / 1000));
+				if (e.target.classList.length && e.target.getAttribute("class").match(/(danmaku\-content|user\-name)/)) {
+					setTimeout(() => {	// 因为无法准确的确定窗口的状态，加延迟防止弹窗消失时时间突变
+						let ts = new Date(e.target.parentElement.getAttribute("data-ts") * 1000);
+						document.querySelector("#send-ts").innerText = ts.toLocaleString('chinese', { hour12: false });
+						document.querySelector("#send-ts").innerHTML += "<br/>" + formatDate(Math.floor((new Date() - ts) / 1000));
+					}, 50);
 				}
 			}, true);
 
