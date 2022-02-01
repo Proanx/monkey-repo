80b60e9b8be7d144b7f857180fc6ab44f303694f
Pronax <skyfire.han@gmail.com>
2022-02-01 13:52:51 +0800

    修复一些不同步的bug，减去了一些无意义的提示

--- bilibiliLiveNotifaction.user.js
+++ bilibiliLiveNotifaction.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站直播通知
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  需要有至少一个b站页面开在后台，在有页面常驻的情况下提醒延迟不超过3分钟
 // @author       Pronax
 // @match        https://*.bilibili.com/*
@@ -17,13 +17,13 @@
 (function () {
 	'use strict';
 
-	const tabId = giveMeAName();
+	const TAB_ID = giveMeAName();
 
 	var isConsumer = false;
 
 	// list存的都是uid
-	var onlineList = [];
-	var blackList = [];
+	var onlineList;
+	var blockList;
 
 	var interval;
 
@@ -49,13 +49,15 @@
 	init();
 
 	function init() {
-		console.log("开始尝试竞争锁");
 		try {
+			if(typeof(__NEPTUNE_IS_MY_WAIFU__)!='undefined'){
+				updateBlockList(__NEPTUNE_IS_MY_WAIFU__.roomInitRes.data.uid);
+			}
+			console.log("开始尝试竞争锁");
 			let consumer = tryConsumer();
 			console.log("竞争成功");
 			if (consumer && Date.now() - consumer.lastHeartbeat <= listExpireTime) {
 				console.log("旧数据有效");
-				onlineList = getList();
 			} else {
 				console.log("数据过期，重新获取新数据");
 				GetLiveList(true);
@@ -85,16 +87,17 @@
 
 	function GetLiveList(isInit) {
 		ajax(`https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/w_live_users?size=100`, function (result) {
+			onlineList = getList("onlineList");
+			blockList = getList("blockList");
 			if (result.code == result.message) {
-				let tempList = [];
 				for (let item of result.data.items) {
-					if ((!isInit) && onlineList.indexOf(item.uid) < 0 && blackList.indexOf(item.uid) < 0) {
+					if (!(isInit || onlineList.has(item.uid) || blockList.has(item.uid))) {
 						console.log("发送" + item.uname + "的开播通知");
 						notify(item.uid, item.uname, item.title, item.face, item.link);
 					}
-					tempList.push(item.uid);
+					onlineList.add(item.uid);
+					blockList.delete(item.uid);
 				}
-				onlineList = tempList;
 				saveList();
 			}
 		});
@@ -130,9 +133,9 @@
 	function heartbeat(timestamp = Date.now()) {
 		console.log("开始更新心跳");
 		let consumer = GM_getValue("consumer");
-		if (consumer && consumer.name == tabId) {
+		if (consumer && consumer.name == TAB_ID) {
 			console.log("心跳更新成功");
-			GM_setValue("consumer", { "name": tabId, "lastHeartbeat": timestamp });
+			GM_setValue("consumer", { "name": TAB_ID, "lastHeartbeat": timestamp });
 			changeTabTitle();
 			return true;
 		}
@@ -144,24 +147,32 @@
 
 	function tryConsumer() {
 		let consumer = GM_getValue("consumer");
-		if (consumer && consumer.name != tabId && Date.now() - consumer.lastHeartbeat <= consumerExpireTime) {
+		if (consumer && consumer.name != TAB_ID && Date.now() - consumer.lastHeartbeat <= consumerExpireTime) {
 			console.log("已存在消费者且未过期，过期时间：", (consumerExpireTime - (Date.now() - consumer.lastHeartbeat)) / 1000);
 			changeTabTitle();
 			throw new Error("已有线程正在生产");
 		}
-		GM_setValue("consumer", { "name": tabId, "lastHeartbeat": Date.now() });
+		GM_setValue("consumer", { "name": TAB_ID, "lastHeartbeat": Date.now() });
 		isConsumer = true;
 		changeTabTitle();
 		return consumer;
 	}
 
-	function getList() {
-		let list = GM_getValue("onlineList");
-		return list ? list.split(",").map(Number) : [];
+	function getList(name) {
+		let list = GM_getValue(name);
+		list = list ? list.split(",").map(Number) : [];
+		return new Set(list);
 	}
 
-	function saveList() {
-		GM_setValue("onlineList", onlineList.toString());
+	function saveList(name) {
+		GM_setValue("onlineList", Array.from(onlineList).toString());
+		GM_setValue("blockList", Array.from(blockList).toString());
+	}
+	
+	function updateBlockList(uid){
+		let list = getList("blockList");
+		list.add(uid);
+		GM_setValue("blockList", Array.from(list).toString());
 	}
 
 	function giveMeAName() {
