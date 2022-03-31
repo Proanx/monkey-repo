92fa388970de46cb44ad83133776f0e5ff5ee0dc
韩松 <Pronax.han@gmail.com>
2022-03-31 19:52:00 +0800

    feat: 添加礼物数量图标，在代码里面添加了一个新的变量用于控制领红包的门槛

--- bililiveredpacketrebot.user.js
+++ bililiveredpacketrebot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            B站直播自动抢红包
-// @version         0.1.9
+// @version         0.1.10
 // @description     会在进房间以后的下一次发红包时开始生效
 // @author          Pronax
 // @include         /https:\/\/live\.bilibili\.com\/(blanc\/)?\d+/
@@ -19,18 +19,32 @@
 
     if (!document.cookie.match(/bili_jct=(\w*); /)) { return; }
 
-    const TITLE_ICON = "🧧 ";
+    // 抢红包门槛，只有红包价值大于等于门槛的时候才会抢
+    // 单位是电池
+    const doorSill = 0;
+
+    const RED_PACKET_ICON = "🧧";
+    const GIFT_ICON = "🎁";
     const JCT = document.cookie.match(/bili_jct=(\w*); /)[1];
     const MY_ID = document.cookie.match(/DedeUserID=(\d+);/)[1];
     const ROOM_ID = await ROOM_INFO_API.getRid();
     const ROOM_USER_ID = await ROOM_INFO_API.getUid();
     const FOLLOWED = await getFollowStatus(ROOM_USER_ID);
 
+    window.addEventListener('focus', e => {
+        giftCount = 0;
+        setTimeout(() => {
+            updateTabTitle();
+        }, 3000);
+    });
+
     // 通知css    
     GM_addStyle(".noticejs-link{margin-right:15px}.noticejs-top{top:0;width:100%!important}.noticejs-top .item{border-radius:0!important;margin:0!important}.noticejs-topRight{top:10px;right:10px}.noticejs-topLeft{top:10px;left:10px}.noticejs-topCenter{top:10px;left:50%;transform:translate(-50%)}.noticejs-middleLeft,.noticejs-middleRight{right:10px;top:50%;transform:translateY(-50%)}.noticejs-middleLeft{left:10px}.noticejs-middleCenter{top:50%;left:50%;transform:translate(-50%,-50%)}.noticejs-bottom{bottom:0;width:100%!important}.noticejs-bottom .item{border-radius:0!important;margin:0!important}.noticejs-bottomRight{bottom:10px;right:10px}.noticejs-bottomLeft{bottom:10px;left:10px}.noticejs-bottomCenter{bottom:10px;left:50%;transform:translate(-50%)}.noticejs{opacity:0.85;font-size: 14px;font-family:Helvetica Neue,Helvetica,Arial,sans-serif}.noticejs .item{margin:0 0 10px;border-radius:5px;overflow:hidden}.noticejs .item .close{cursor:pointer;width:21px;height:21px;text-align: center;margin-top: -3px;margin-right: -3px;float:right;font-size:18px;font-weight:700;line-height:1;color:#fff;text-shadow:0 1px 0 #fff;opacity:1;}.noticejs .item .close:hover{opacity:.5;color:#000}.noticejs .item a{color:#fff;border-bottom:1px dashed #fff}.noticejs .item a,.noticejs .item a:hover{text-decoration:none}.noticejs .success{background-color:#64ce83}.noticejs .success .noticejs-heading{background-color:#3da95c;color:#fff;padding:5px}.noticejs .success .noticejs-body{color:#fff;padding:10px}.noticejs .success .noticejs-body:hover{visibility:visible!important}.noticejs .success .noticejs-content{visibility:visible;word-break:break-all;}.noticejs .info{background-color:#3ea2ff}.noticejs .info .noticejs-heading{background-color:#067cea;color:#fff;padding:5px}.noticejs .info .noticejs-body{color:#fff;padding:10px}.noticejs .info .noticejs-body:hover{visibility:visible!important}.noticejs .info .noticejs-content{visibility:visible;word-break:break-all;}.noticejs .warning{background-color:#ff7f48}.noticejs .warning .noticejs-heading{background-color:#f44e06;color:#fff;padding:5px}.noticejs .warning .noticejs-body{color:#fff;padding:10px}.noticejs .warning .noticejs-body:hover{visibility:visible!important}.noticejs .warning .noticejs-content{visibility:visible;word-break:break-all;}.noticejs .error{background-color:#e74c3c}.noticejs .error .noticejs-heading{background-color:#ba2c1d;color:#fff;padding:5px}.noticejs .error .noticejs-body{color:#fff;padding:10px}.noticejs .error .noticejs-body:hover{visibility:visible!important}.noticejs .error .noticejs-content{visibility:visible;word-break:break-all;}.noticejs .progressbar{width:100%}.noticejs .progressbar .bar{width:1%;height:30px;background-color:#4caf50}.noticejs .success .noticejs-progressbar{width:100%;background-color:#64ce83;margin-top:-1px}.noticejs .success .noticejs-progressbar .noticejs-bar{width:100%;height:5px;background:#3da95c}.noticejs .info .noticejs-progressbar{width:100%;background-color:#3ea2ff;margin-top:-1px}.noticejs .info .noticejs-progressbar .noticejs-bar{width:100%;height:5px;background:#067cea}.noticejs .warning .noticejs-progressbar{width:100%;background-color:#ff7f48;margin-top:-1px}.noticejs .warning .noticejs-progressbar .noticejs-bar{width:100%;height:5px;background:#f44e06}.noticejs .error .noticejs-progressbar{width:100%;background-color:#e74c3c;margin-top:-1px}.noticejs .error .noticejs-progressbar .noticejs-bar{width:100%;height:5px;background:#ba2c1d}@keyframes noticejs-fadeOut{0%{opacity:1}to{opacity:0}}.noticejs-fadeOut{animation-name:noticejs-fadeOut}@keyframes noticejs-modal-in{to{opacity:.3}}@keyframes noticejs-modal-out{to{opacity:0}}.noticejs-rtl .noticejs-heading{direction:rtl}.noticejs-rtl .close{float:left!important;margin-left:7px;margin-right:0!important}.noticejs-rtl .noticejs-content{direction:rtl}.noticejs{position:fixed;z-index:10050;width:320px}.noticejs ::-webkit-scrollbar{width:8px}.noticejs ::-webkit-scrollbar-button{width:8px;height:5px}.noticejs ::-webkit-scrollbar-track{border-radius:10px}.noticejs ::-webkit-scrollbar-thumb{background:hsla(0,0%,100%,.5);border-radius:10px}.noticejs ::-webkit-scrollbar-thumb:hover{background:#fff}.noticejs-modal{position:fixed;width:100%;height:100%;background-color:#000;z-index:10000;opacity:.3;left:0;top:0}.noticejs-modal-open{opacity:0;animation:noticejs-modal-in .3s ease-out}.noticejs-modal-close{animation:noticejs-modal-out .3s ease-out;animation-fill-mode:forwards}");
 
     var notice;
     var timeout;
+    var giftCount = 0;
+    var unpacking = false;
 
     var formData = new FormData();
     formData.set("csrf", JCT);
@@ -42,7 +56,11 @@
     formData.set("ruid", ROOM_USER_ID);
     formData.set("spm_id", "444.8.red_envelope.extract");
 
-    bliveproxy.addCommandHandler("POPULARITY_RED_POCKET_START", drawRadPacket);
+    bliveproxy.addCommandHandler("POPULARITY_RED_POCKET_START", (message) => {
+        if (doorSill >= (message.data.total_price / 100)) {
+            setTimeout(drawRadPacket(message), Math.random() * 10000);
+        }
+    });
     bliveproxy.addCommandHandler("POPULARITY_RED_POCKET_WINNER_LIST", radPacketWinner);
 
     window.addEventListener('beforeunload', (event) => {
@@ -77,7 +95,8 @@
                     clearTimeout(timeout);
                     timeout = null;
                     notice = showMessage(`坐等 ${message.data.sender_name} 的红包开奖<br>红包ID：${message.data.lot_id}`, "info", "啊哈哈哈哈哈哈，红包来咯", message.data.last_time * 10);
-                    changeTabTitle(true);
+                    unpacking = true;
+                    updateTabTitle();
                 }
             });
     }
@@ -114,13 +133,15 @@
     }
 
     function radPacketWinner(message) {
-        changeTabTitle(false);
+        unpacking = false;
         for (let winner of message.data.winner_info) {
             if (MY_ID == winner.uid) {
                 showMessage(`抽中了${winner.award_name} *${winner.gift_num}个！`, "success", "中奖啦！", false);
+                giftCount++;
                 break;
             }
         }
+        updateTabTitle();
         if (!FOLLOWED) {
             timeout = setTimeout(async () => {
                 let unfollowed = await unfollow();
@@ -150,8 +171,22 @@
         }).show();
     }
 
-    function changeTabTitle(flag) {
-        document.title = (flag ? TITLE_ICON : "") + document.title.replace(TITLE_ICON, "");
+    function updateTabTitle() {
+        let title = document.title.replace(/(🧧 🎁\*\d* )|(🧧 )|(🎁\*\d* )/, "");
+        let header = "";
+        if (unpacking) {
+            header += RED_PACKET_ICON;
+        }
+        if (giftCount > 0) {
+            if (header) {
+                header += " ";
+            }
+            header += GIFT_ICON + "*" + giftCount;
+        }
+        if (header) {
+            header += " ";
+        }
+        document.title = header + title;
     }
 
     async function getFollowStatus(uid) {
