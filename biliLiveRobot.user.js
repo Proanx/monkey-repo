787c3f99b7e3baeb8ec5b02eda702ca2350dd479
韩松 <Pronax.han@gmail.com>
2022-07-23 20:51:53 +0800

    feat: 新增两个欢迎语句

--- biliLiveRobot.user.js
+++ biliLiveRobot.user.js
@@ -12,200 +12,14 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // @noframes
+// @require      https://greasyfork.org/scripts/434638-xfgryujk-s-bliveproxy/code/xfgryujk's%20bliveproxy.js?version=983438
 // ==/UserScript==
-
-// 不登录用锤子呢
-if (!document.cookie.match(/bili_jct=(\w*); /)) { return; }
-
-(function () {
-    const HEADER_SIZE = 16;
-
-    const WS_BODY_PROTOCOL_VERSION_NORMAL = 0;
-    const WS_BODY_PROTOCOL_VERSION_HEARTBEAT = 1;
-    const WS_BODY_PROTOCOL_VERSION_DEFLATE = 2;
-    const WS_BODY_PROTOCOL_VERSION_BROTLI = 3;
-
-    const OP_HEARTBEAT_REPLY = 3;
-    const OP_SEND_MSG_REPLY = 5;
-
-    let textEncoder = new TextEncoder();
-    let textDecoder = new TextDecoder();
-
-    function main() {
-        if (unsafeWindow.bliveproxy) {
-            // 防止多次加载
-            return;
-        }
-        initApi();
-        hook();
-    }
-
-    function initApi() {
-        unsafeWindow.bliveproxy = api;
-    }
-
-    let api = {
-        addCommandHandler(cmd, handler) {
-            let handlers = this._commandHandlers[cmd];
-            if (!handlers) {
-                handlers = this._commandHandlers[cmd] = [];
-            }
-            handlers.push(handler);
-        },
-        removeCommandHandler(cmd, handler) {
-            let handlers = this._commandHandlers[cmd];
-            if (!handlers) {
-                return;
-            }
-            this._commandHandlers[cmd] = handlers.filter(item => item !== handler);
-        },
-
-        // 私有API
-        _commandHandlers: {},
-        _getCommandHandlers(cmd) {
-            return this._commandHandlers[cmd] || null;
-        }
-    }
-
-    function hook() {
-        unsafeWindow.WebSocket = new Proxy(unsafeWindow.WebSocket, {
-            construct(target, args) {
-                let obj = new target(...args);
-                return new Proxy(obj, proxyHandler);
-            }
-        });
-    }
-
-    let proxyHandler = {
-        get(target, property) {
-            let value = target[property];
-            if ((typeof value) === 'function') {
-                value = value.bind(target);
-            }
-            return value;
-        },
-        set(target, property, value) {
-            if (property === 'onmessage') {
-                let realOnMessage = value;
-                value = function (event) {
-                    myOnMessage(event, realOnMessage);
-                }
-            }
-            target[property] = value;
-            return value;
-        }
-    }
-
-    function myOnMessage(event, realOnMessage) {
-        if (!(event.data instanceof ArrayBuffer)) {
-            realOnMessage(event);
-            return;
-        }
-
-        let data = new Uint8Array(event.data);
-        function callRealOnMessageByPacket(packet) {
-            realOnMessage({ ...event, data: packet });
-        }
-        handleMessage(data, callRealOnMessageByPacket);
-    }
-
-    function makePacketFromCommand(command) {
-        let body = textEncoder.encode(JSON.stringify(command));
-        return makePacketFromUint8Array(body, OP_SEND_MSG_REPLY);
-    }
-
-    function makePacketFromUint8Array(body, operation) {
-        let packLen = HEADER_SIZE + body.byteLength;
-        let packet = new ArrayBuffer(packLen);
-
-        // 不需要压缩
-        let ver = operation === OP_HEARTBEAT_REPLY ? WS_BODY_PROTOCOL_VERSION_HEARTBEAT : WS_BODY_PROTOCOL_VERSION_NORMAL;
-        let packetView = new DataView(packet);
-        packetView.setUint32(0, packLen);        // pack_len
-        packetView.setUint16(4, HEADER_SIZE);    // raw_header_size
-        packetView.setUint16(6, ver);            // ver
-        packetView.setUint32(8, operation);      // operation
-        packetView.setUint32(12, 1);             // seq_id
-
-        let packetBody = new Uint8Array(packet, HEADER_SIZE, body.byteLength);
-        for (let i = 0; i < body.byteLength; i++) {
-            packetBody[i] = body[i];
-        }
-        return packet;
-    }
-
-    function handleMessage(data, callRealOnMessageByPacket) {
-        let offset = 0;
-        while (offset < data.byteLength) {
-            let dataView = new DataView(data.buffer, offset);
-            let packLen = dataView.getUint32(0);
-            if (packLen > 1534026300) { return; }
-            // let rawHeaderSize = dataView.getUint16(4);
-            let ver = dataView.getUint16(6);
-            let operation = dataView.getUint32(8);
-            // let seqId = dataView.getUint32(12);
-
-            let body = new Uint8Array(data.buffer, offset + HEADER_SIZE, packLen - HEADER_SIZE);
-            if (operation === OP_SEND_MSG_REPLY) {
-                switch (ver) {
-                    case WS_BODY_PROTOCOL_VERSION_NORMAL:
-                        body = textDecoder.decode(body);
-                        body = JSON.parse(body);
-                        handleCommand(body, callRealOnMessageByPacket);
-                        break;
-                    case WS_BODY_PROTOCOL_VERSION_DEFLATE:
-                        body = pako.inflate(body);
-                        handleMessage(body, callRealOnMessageByPacket);
-                        break;
-                    case WS_BODY_PROTOCOL_VERSION_BROTLI:
-                        body = BrotliDecode(body);
-                        handleMessage(body, callRealOnMessageByPacket);
-                        break;
-                    default: {
-                        let packet = makePacketFromUint8Array(body, operation);
-                        callRealOnMessageByPacket(packet);
-                        break;
-                    }
-                }
-            } else {
-                let packet = makePacketFromUint8Array(body, operation);
-                callRealOnMessageByPacket(packet);
-            }
-
-            offset += packLen;
-        }
-    }
-
-    function handleCommand(command, callRealOnMessageByPacket) {
-        if (command instanceof Array) {
-            for (let oneCommand of command) {
-                this.handleCommand(oneCommand);
-            }
-            return;
-        }
-
-        let cmd = command.cmd || '';
-        let pos = cmd.indexOf(':');
-        if (pos != -1) {
-            cmd = cmd.substr(0, pos);
-        }
-        let handlers = api._getCommandHandlers(cmd);
-        if (handlers) {
-            for (let handler of handlers) {
-                handler(command);
-            }
-        }
-
-        let packet = makePacketFromCommand(command);
-        callRealOnMessageByPacket(packet);
-    }
-
-    main();
-})();
-
 
 (function () {
     'use strict';
+
+    // 不登录用锤子呢
+    if (!document.cookie.match(/bili_jct=(\w*); /)) { return; }
 
     const JCT = document.cookie.match(/bili_jct=(\w*); /)[1];
     const ROOM_ID = location.href.match(/\/(\d+)/)[1];
@@ -222,6 +36,7 @@ if (!document.cookie.match(/bili_jct=(\w*); /)) { return; }
         "这么宝藏的主播还不关注吗♡",
         "动态里面有歌单，欢迎点歌噢",
         "喜欢主播的可以加下粉丝群♡开播有通知哦",
+        "好听吗，好听就刷点(´∀｀)♡",
     ];
     const GUARD_CONTENT = [              //上舰感谢语 
         "感谢老板上船。这次！绝对不糊涂！",
@@ -236,6 +51,7 @@ if (!document.cookie.match(/bili_jct=(\w*); /)) { return; }
         "哇@$1来啦，欢迎！",
         "@$1来了，给大佬倒茶",
         "欢迎小可爱@$1",
+        "@$1别看了出来聊天",
         "@$1，欢迎入座，看到日落",
     ];
 
